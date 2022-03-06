0820b762947feba370e6add239f2c42608314a8b
Matt Parr <mattparr@google.com>
2022-03-05 20:47:39 -0500

    initial checkin

---
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.leszko.calculator;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ * This is a Javadoc comment
+ */
+
+@Service
+public class Calculator {
+        final static int UMLNUMBER1 = 3;
+	@Cacheable("sum")
+	public int sum(int a, int b) {
+		return a + b;
+	}
+}
